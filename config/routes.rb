08fee902a994d148c90101c7e3aebc9bd2cd89b9
guojie <yingsunyaokong@gmail.com>
2016-12-15 23:13:29 +0800

    i'm the stupid one

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  namespace :account do
+    resources :groups
+  end
   resources :groups do
       member do
         post :join
@@ -10,4 +13,6 @@ Rails.application.routes.draw do
   end
   root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
